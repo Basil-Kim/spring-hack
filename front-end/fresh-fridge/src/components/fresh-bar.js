71b53c5ffd9674f388b8045de473831dab9004a5
Jin Byun <jbyun10@my.bcit.ca>
2022-03-13 09:57:44 -0700

    added the authentication frontend that I tried, erroneous

--- front-end/fresh-fridge/src/components/fresh-bar.js
+++ front-end/fresh-fridge/src/components/fresh-bar.js
@@ -1,14 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import Bar from "./Bar";
+import './ListItem.css';
 
-const Bar = (props) => {
-  const { bgcolor, completed } = props;
+function FreshBar(){
+  const [completed, setCompleted] = useState(0);
+
+  useEffect(() => {
+    setInterval(() => setCompleted(Math.floor(Math.random() * 100) + 1), 2000);
+  }, []);
+
   return (
-    <div>
-      <div>
-        <span>{`${completed}%`}</span>
-      </div>
-    </div>
-  );
-};
 
-export default ProgressBar;
+
+    <div id="card">
+      <div id="circle"></div>
+
+      <div id="item">Placeholder</div>
+     
+        <Bar bgcolor={"#6a1b9a"} completed={completed} />
+
+      
+        
+        </div>
+     
+   
+
+      
+
+  );
+}
+
+export default FreshBar;
