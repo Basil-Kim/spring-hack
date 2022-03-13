2ce939efc85f66a0e725aaa73924be6ef4abfad6
Jin Byun <jbyun10@my.bcit.ca>
2022-03-13 09:54:02 -0700

    added what I have done for authentication

--- front-end/fresh-fridge/src/App.js
+++ front-end/fresh-fridge/src/App.js
@@ -1,13 +1,46 @@
 import './App.css';
+import React, { useState } from 'react';
+import {Routes, BrowserRouter, Route} from 'react-router-dom'
 import AddListItem from './components/AddListItem';
+import Login from './components/login'
+import Register from './components/register'
 
 function App() {
+
+  const [errormsg, setErrormsg] = useState({});
+  const [user,setLoginUser] = useState({});
+  const renderErrormsg = (name) =>
+  name === errormsg.name && (
+    <div className='error'>{errormsg.message}</div>
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
+            <BrowserRouter>
+        <Routes>
+          <Route path='/'>
+            {
+              user && user._id ? <Home/> : <Login/>
+            }<Home/>
+          </Route>
+          <Route path='/Login'>
+            <Login setLoginUser={setLoginUser}/>
+          </Route>
+          <Route path='/Register'>
+            <Register/>
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    <Addition/>  
+    </div>
+  );
+}
+
+function Addition() {
+  return (
+    <header className="App-header">
         <AddListItem/>
       </header>
-    </div>
   );
 }
 
